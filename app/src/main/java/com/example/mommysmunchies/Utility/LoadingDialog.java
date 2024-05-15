c01e3f1b3b97148b53ba725328b20072c4da0d36
wajida-asha <asha.wajida@gmail.com>
2024-05-15 23:04:01 +1000

    KAN-28:SignUp and Login Using Firebase Verification

---
+++ app/src/main/java/com/example/mommysmunchies/Utility/LoadingDialog.java
@@ -0,0 +1,35 @@
+package com.example.mommysmunchies.Utility;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.LayoutInflater;
+
+import com.example.mommysmunchies.R;
+import com.example.mommysmunchies.databinding.DialogLayoutBinding;
+
+
+;
+
+public class LoadingDialog {
+    private Activity activity;
+    private AlertDialog alertDialog;
+
+    public LoadingDialog(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void startLoading() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity, R.style.loadingDialogStyle);
+        DialogLayoutBinding binding = DialogLayoutBinding.inflate(LayoutInflater.from(activity), null, false);
+        builder.setView(binding.getRoot());
+        builder.setCancelable(false);
+        alertDialog = builder.create();
+        binding.rotateLoading.start();
+        alertDialog.show();
+    }
+
+    public void stopLoading() {
+        alertDialog.dismiss();
+    }
+
+}
