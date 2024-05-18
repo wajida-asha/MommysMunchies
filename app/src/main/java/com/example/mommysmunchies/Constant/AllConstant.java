a2152aca6bbbec08c84a5066d46e87a844d2da45
wajida-asha <asha.wajida@gmail.com>
2024-05-18 18:57:26 +1000

    KAN-30

--- app/src/main/java/com/example/mommysmunchies/Constant/AllConstant.java
+++ app/src/main/java/com/example/mommysmunchies/Constant/AllConstant.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 public class AllConstant {
     public static final int STORAGE_REQUEST_CODE =1000 ;
     public static final String IMAGE_PATH = "/Profile/image_profile.jpg";
+    public static int LOCATION_REQUEST_CODE = 2000;
 
     public static ArrayList<PlaceModel> placesName = new ArrayList<>(
             Arrays.asList(
